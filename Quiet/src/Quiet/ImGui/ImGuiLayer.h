d72fcf6a43d24e9c52596366f74c0b43fa5f675b
Gaston M. Pannuto <gastonpannuto@gmail.com>
2023-02-01 15:24:38 -0300

    ImGui Events

--- Quiet/src/Quiet/ImGui/ImGuiLayer.h
+++ Quiet/src/Quiet/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Quiet/Core/Layer.h"
 
+#include "Quiet/Events/ApplicationEvent.h"
+#include "Quiet/Events/KeyEvent.h"
+#include "Quiet/Events/MouseEvent.h"
+
 namespace Quiet
 {
 	class QUIET_API ImGuiLayer final : public Layer
@@ -14,6 +18,17 @@ namespace Quiet
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 	};
