acff1a389a790b153766c90b104520e70b5164af
Gaston M. Pannuto <gastonpannuto@gmail.com>
2023-02-08 18:05:52 -0300

    Transforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,6 +2,7 @@
 
 #include "ImGui/imgui.h"
 #include "Quiet/Events/KeyEvent.h"
+
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
@@ -37,10 +38,10 @@ public:
 		m_SquareVA.reset(Quiet::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Quiet::VertexBuffer> squareVB;
@@ -60,22 +61,28 @@ public:
 			
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
+
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;			
+
 			out vec3 v_Position;
 			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 		std::string fragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
 			in vec4 v_Color;
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
@@ -89,12 +96,16 @@ public:
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
+
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -115,7 +126,7 @@ public:
 	void OnUpdate(Quiet::Timestep ts) override
 	{
 		// Set debug Timestep 
-		debugTS = ts;
+		m_DebugTS = ts;
 
 		// Camera Movement
 		if (Quiet::Input::IsKeyPressed(Quiet::Key::Left))	
@@ -128,11 +139,10 @@ public:
 			m_CameraPosition.y -= m_CameraMovementSpeed * ts;
 
 		// Camera Rotation
-		if (Quiet::Input::IsKeyPressed(Quiet::Key::A))
+		if (Quiet::Input::IsKeyPressed(Quiet::Key::Q))
 			m_CameraRotation -= m_CameraRotationSpeed;
-		if (Quiet::Input::IsKeyPressed(Quiet::Key::D))
+		if (Quiet::Input::IsKeyPressed(Quiet::Key::E))
 			m_CameraRotation += m_CameraRotationSpeed;
-
 
 		Quiet::RendererCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Quiet::RendererCommand::Clear();
@@ -142,7 +152,18 @@ public:
 
 		Quiet::Renderer::BeginScene(m_Camera);
 
-		Quiet::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Quiet::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+
+		}
 		Quiet::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Quiet::Renderer::EndScene();
@@ -150,9 +171,9 @@ public:
 
 	void OnImGuiRender() override
 	{
-		ImGui::Begin("Dialog");
+		ImGui::Begin("Test");
 		ImGui::TextColored({ 0.3f, 0.4f, 0.8f, 1.0f }, "Debug");
-		ImGui::Text("Deltatime: %f s (%f ms)", debugTS, debugTS.GetMilliseconds());
+		ImGui::Text("Deltatime: %f s (%f ms)", m_DebugTS, m_DebugTS.GetMilliseconds());
 		ImGui::Separator();
 
 		ImGui::TextColored({ 0.3f, 0.4f, 0.8f, 1.0f }, "Camera Status");
@@ -173,16 +194,7 @@ public:
 
 	void OnEvent(Quiet::Event& event) override
 	{
-		Quiet::EventDispatcher dispatcher(event);
-		dispatcher.Dispatch<Quiet::KeyPressedEvent>(BIND_EVENT_FN(TestLayer::OnKeyPressedEvent));
-	}
-
-	bool OnKeyPressedEvent(Quiet::KeyPressedEvent& event)
-	{
-
-
 		
-		return false;
 	}
 
 private:
@@ -200,7 +212,7 @@ private:
 	float m_CameraRotationSpeed = 0.5f;
 
 	// Debug
-	Quiet::Timestep debugTS;
+	Quiet::Timestep m_DebugTS;
 };
 
 class Sandbox : public Quiet::Application
