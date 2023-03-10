d34945ae746098c9f689fd43cc6db51878344b15
Gaston M. Pannuto <gastonpannuto@gmail.com>
2023-03-10 13:48:30 -0300

    Log Serialization

--- Quiet/src/Quiet/Core/Log.cpp
+++ Quiet/src/Quiet/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "Quiet/Core/Log.h"
 
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Quiet
 {
@@ -10,15 +11,25 @@ namespace Quiet
 
 	void Log::Init()
 	{
+		// Serialize Log
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Quiet.log", true));
+
 		// Set Format of the Logging
-		spdlog::set_pattern("%^[%T] %n::%v%$");
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
 		// Core Logger Parameters
-		s_CoreLogger = spdlog::stdout_color_mt("QUIET");
+		s_CoreLogger = std::make_shared<spdlog::logger>("QUIET", begin(logSinks), end(logSinks));
+		register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
 
 		// Client Logger Parameters
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
