fe1789ae67319eaf90b16756f389ef58ed6bb923
Panuh404 <gastonpannuto@gmail.com>
2024-02-03 13:03:35 -0300

    Utility Controls
    - Number Box
    - Scalar Box
    - Vector Box

---
+++ Editor/Utilities/Controls/VectorBox.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using static QEditor.Utilities.Controls.VectorBox;
+
+namespace QEditor.Utilities.Controls
+{
+    public enum VectorType
+    {
+        Vector2,
+        Vector3,
+        Vector4,
+    }
+
+    class VectorBox : Control
+    {
+        // PROPERTY: Vector Type
+        public VectorType VectorType
+        {
+            get => (VectorType)GetValue(VectorTypeProperty);
+            set => SetValue(VectorTypeProperty, value);
+        }
+        public static readonly DependencyProperty VectorTypeProperty =
+            DependencyProperty.Register(nameof(VectorType), typeof(VectorType), typeof(VectorBox),
+                new PropertyMetadata(VectorType.Vector3));
+
+        // PROPERTY: Orientation
+        public Orientation Orientation
+        {
+            get => (Orientation)GetValue(OrientationProperty);
+            set => SetValue(OrientationProperty, value);
+        }
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(VectorBox),
+                new PropertyMetadata(Orientation.Horizontal));
+
+        // PROPERTY: Multiplier
+        public double Multiplier
+        {
+            get => (double)GetValue(MultiplierProperty);
+            set => SetValue(MultiplierProperty, value);
+        }
+        public static readonly DependencyProperty MultiplierProperty =
+            DependencyProperty.Register(nameof(Multiplier), typeof(double), typeof(VectorBox),
+                new PropertyMetadata(1.0));
+
+        // PROPERTY: VECTOR VALUE X
+        public string X
+        {
+            get => (string)GetValue(XProperty);
+            set => SetValue(XProperty, value);
+        }
+        public static readonly DependencyProperty XProperty =
+            DependencyProperty.Register(nameof(X), typeof(string), typeof(VectorBox),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        // PROPERTY: VECTOR VALUE Y
+        public string Y
+        {
+            get => (string)GetValue(YProperty);
+            set => SetValue(YProperty, value);
+        }
+        public static readonly DependencyProperty YProperty =
+            DependencyProperty.Register(nameof(Y), typeof(string), typeof(VectorBox),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        // PROPERTY: VECTOR VALUE Z
+        public string Z
+        {
+            get => (string)GetValue(ZProperty);
+            set => SetValue(ZProperty, value);
+        }
+        public static readonly DependencyProperty ZProperty =
+            DependencyProperty.Register(nameof(Z), typeof(string), typeof(VectorBox),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        // PROPERTY: VECTOR VALUE W
+        public string W
+        {
+            get => (string)GetValue(WProperty);
+            set => SetValue(WProperty, value);
+        }
+        public static readonly DependencyProperty WProperty =
+            DependencyProperty.Register(nameof(W), typeof(string), typeof(VectorBox),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+        static VectorBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(VectorBox),
+                new FrameworkPropertyMetadata(typeof(VectorBox)));
+        }
+    }
+}
